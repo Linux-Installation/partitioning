0c55703a33134a369f8ffe0be74af21f20d9317e
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2022-10-18 22:40:33 +0200

    Make programs partition bigger

--- 22.04-live-1.sh
+++ 22.04-live-1.sh
@@ -4,6 +4,6 @@ partition="/dev/sda2"
 pvcreate $partition
 vgcreate $vg $partition
 sudo lvcreate -L 30G -n root $vg
-sudo lvcreate -L 10G -n programs $vg
+sudo lvcreate -L 15G -n programs $vg
 sudo lvcreate -L 1G -n swap $vg
 sudo lvcreate -l 100%FREE -n home $vg
