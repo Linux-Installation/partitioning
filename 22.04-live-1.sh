ffa00ef73e93fd08ec101446a007772af40dd669
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2022-10-28 14:25:39 +0200

    add sudo

--- 22.04-live-1.sh
+++ 22.04-live-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 vg="ubuntu-mate"
 partition="/dev/sda2"
-pvcreate $partition
-vgcreate $vg $partition
+sudo pvcreate $partition
+sudo vgcreate $vg $partition
 sudo lvcreate -L 30G -n root $vg
 sudo lvcreate -L 15G -n programs $vg
 sudo lvcreate -L 1G -n swap $vg
