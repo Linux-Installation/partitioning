43f3f5512035952f14c3a5418a4a004ac8191e28
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2024-05-25 17:39:34 +0200

    bigger root and programs partition, let some space free if later needed

--- 22.04-live-1.sh
+++ 22.04-live-1.sh
@@ -3,7 +3,7 @@ vg="ubuntu-mate"
 partition="/dev/sda2"
 pvcreate $partition
 vgcreate $vg $partition
-sudo lvcreate -L 30G -n root $vg
-sudo lvcreate -L 10G -n programs $vg
+sudo lvcreate -L 40G -n root $vg
+sudo lvcreate -L 20G -n programs $vg
 sudo lvcreate -L 1G -n swap $vg
-sudo lvcreate -l 100%FREE -n home $vg
+sudo lvcreate -l 80%FREE -n home $vg
