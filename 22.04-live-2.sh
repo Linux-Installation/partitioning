4e0798a7dec497d5d2309d59fa52884aaf83f034
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2024-05-25 19:07:23 +0200

    move /var/lib/snap aswell

--- 22.04-live-2.sh
+++ 22.04-live-2.sh
@@ -5,11 +5,13 @@ sudo mkdir -p /media/root
 sudo mount /dev/${vg}/root /media/root
 sudo mkdir -p /media/programs
 sudo mount /dev/${vg}/programs /media/programs
-sudo mkdir -p /media/root/programs
+sudo mkdir -p /media/root/programs/var-lib/
 sudo mkdir /media/programs/snap
 sudo mv /media/root/snap/* /media/programs/snap/
 sudo mv /media/root/var/lib/flatpak /media/programs/
-sudo mv /media/root/var/lib/snapd /media/programs/
-sudo ln -s /programs/flatpak /media/root/var/lib/flatpak
+sudo mv /media/root/var/lib/snapd/* /media/programs/var-lib/snapd
+sudo mv /media/root/var/lib/snap/* /media/programs/var-lib/snap
+sudo ln -s /programs/var-lib/flatpak /media/root/var/lib/flatpak
 sudo su -c 'echo "/programs/snap /snap   none    bind    0   0" >> /media/root/etc/fstab'
-sudo su -c 'echo "/programs/snapd /var/lib/snapd none    bind    0   0" >> /media/root/etc/fstab'
+sudo su -c 'echo "/programs/var-lib/snapd /var/lib/snapd none    bind    0   0" >> /media/root/etc/fstab'
+sudo su -c 'echo "/programs/var-lib/snap /var/lib/snap none    bind    0   0" >> /media/root/etc/fstab'
