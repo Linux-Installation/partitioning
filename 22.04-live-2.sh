a850f58cadaf1e733d58593c39a94a080e560370
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2024-05-26 00:59:45 +0200

    Move more Snap folders to programs

--- 22.04-live-2.sh
+++ 22.04-live-2.sh
@@ -5,13 +5,15 @@ sudo mkdir -p /media/root
 sudo mount /dev/${vg}/root /media/root
 sudo mkdir -p /media/programs
 sudo mount /dev/${vg}/programs /media/programs
-sudo mkdir -p /media/root/programs/var-lib/
+sudo mkdir -p /media/root/programs/
 sudo mkdir /media/programs/snap
+sudo mkdir -p /media/programs/var/lib/snapd
+sudo mkdir -p /media/programs/var/snap
 sudo mv /media/root/snap/* /media/programs/snap/
-sudo mv /media/root/var/lib/flatpak /media/programs/
-sudo mv /media/root/var/lib/snapd/* /media/programs/var-lib/snapd
-sudo mv /media/root/var/lib/snap/* /media/programs/var-lib/snap
-sudo ln -s /programs/var-lib/flatpak /media/root/var/lib/flatpak
+sudo mv /media/root/var/lib/flatpak /media/programs/var/lib/
+sudo mv /media/root/var/lib/snapd/* /media/programs/var/lib/snapd
+sudo mv /media/root/var/snap/* /media/programs/var/snap
+sudo ln -s /programs/var/lib/flatpak /media/root/var/lib/flatpak
 sudo su -c 'echo "/programs/snap /snap   none    bind    0   0" >> /media/root/etc/fstab'
-sudo su -c 'echo "/programs/var-lib/snapd /var/lib/snapd none    bind    0   0" >> /media/root/etc/fstab'
-sudo su -c 'echo "/programs/var-lib/snap /var/lib/snap none    bind    0   0" >> /media/root/etc/fstab'
+sudo su -c 'echo "/programs/var/lib/snapd /var/lib/snapd none    bind    0   0" >> /media/root/etc/fstab'
+sudo su -c 'echo "/programs/var/snap /var/snap none    bind    0   0" >> /media/root/etc/fstab'
