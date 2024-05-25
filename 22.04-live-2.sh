7db963c17a6c3c12183949700ae237048df39920
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2024-05-25 18:29:40 +0200

    move snapd to programs partition

--- 22.04-live-2.sh
+++ 22.04-live-2.sh
@@ -7,7 +7,10 @@ sudo mkdir -p /media/programs
 sudo mount /dev/${vg}/programs /media/programs
 sudo mkdir -p /media/root/programs
 sudo mkdir /media/programs/snap
+sudo mkdir /media/programs/snapd
 sudo mv /media/root/snap/* /media/programs/snap/
+sudo mv /media/root/var/lib/snapd/* /media/programs/snapd/
 sudo mv /media/root/var/lib/flatpak /media/programs/
 sudo ln -s /programs/flatpak /media/root/var/lib/flatpak
+sudo ln -s /programs/snapd /media/root/var/lib/snapd
 sudo su -c 'echo "/programs/snap /snap   none    bind    0   0" >> /media/root/etc/fstab'
